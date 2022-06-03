fa58404e155d43b6170882e6f0265e0777fb3c6a
zty-f <2559247260@qq.com>
2022-06-03 15:10:26 +0800

    2022.6.3 数据库创建sql补充

---
+++ doc/tiktok.sql
@@ -0,0 +1,56 @@
+-- User表
+CREATE TABLE `users`
+(
+    `id`             bigint(0) NOT NULL AUTO_INCREMENT COMMENT 'PK',
+    `name`           varchar(32)  NOT NULL,
+    `password`       varchar(32)  NULL DEFAULT NULL,
+    `follow_count`   bigint(0)  NULL DEFAULT 0,
+    `follower_count` bigint(0)  NULL DEFAULT 0,
+    `is_follow`      tinyint(0)  NULL DEFAULT 0,
+    PRIMARY KEY (`id`)
+)
+-- 视频表
+CREATE TABLE `videos`
+(
+    `id`             bigint(0) NOT NULL  COMMENT 'video_id',
+    `play_url`       varchar(200)   DEFAULT NULL,
+    `cover_url`      varchar(200)  DEFAULT NULL,
+    `favorite_count` bigint(0) NULL DEFAULT 0 COMMENT '点赞量',
+    `comment_count`  bigint(0) NULL DEFAULT 0 COMMENT '评论量',
+    `title`          varchar(128)  DEFAULT '' COMMENT '标题',
+    `user_id`        bigint(0) NOT NULL COMMENT 'FK reference user id',
+    `is_favorite`    tinyint(0) NULL DEFAULT 0,
+    `create_time`    datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP (0),
+    PRIMARY KEY (`id`)
+)
+
+-- 评论表
+CREATE TABLE `comments`
+(
+    `id`          bigint(0) NOT NULL AUTO_INCREMENT,
+    `user_id`     varchar(40)  DEFAULT '',
+    `video_id`    varchar(40)  DEFAULT '',
+    `content`     varchar(500) DEFAULT '',
+    `create_date` varchar(200) DEFAULT '',
+    PRIMARY KEY (`id`)
+)
+
+-- 点赞表
+CREATE TABLE `favorites`
+(
+    `id`          bigint(0) NOT NULL AUTO_INCREMENT,
+    `user_id`     bigint(0) NULL DEFAULT NULL,
+    `video_id`    bigint(0) NULL DEFAULT NULL,
+    `is_favorite` tinyint(0) NULL DEFAULT 0,
+    PRIMARY KEY (`id`)
+)
+
+-- 关注表
+CREATE TABLE `relations`
+(
+    `id`           bigint(0) NOT NULL AUTO_INCREMENT,
+    `user_id`      bigint(0) NULL DEFAULT 0,
+    `following_id` bigint(0) NULL DEFAULT 0,
+    `is_follow`    tinyint(0) NULL DEFAULT 0,
+    PRIMARY KEY (`id`) USING BTREE
+)
